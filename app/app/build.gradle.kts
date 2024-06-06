58241dd7bc77e88383b880f1b48628dd2100a3df
Alexander Linder <seine@eloque.nz>
2024-06-06 12:10:53 +0200

    Bump version to v0.2.0

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "nz.eloque.justshop"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
